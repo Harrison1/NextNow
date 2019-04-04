86ddd2c2825fa7881948716521c2812c3c9ac92c
Harrison1 <hmcguire13@gmail.com>
2019-04-04 07:15:11 -0400

    added trilogy components

--- containers/App.tsx
+++ containers/App.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import Hero from 'trilogycomponents/core/Hero';
+import HeroContent from 'trilogycomponents/core/HeroContent';
 
 type Props = {
   content?: object,
@@ -8,7 +10,17 @@ const App: React.FunctionComponent<Props> = ({ content }) => {
   console.log(content)
 
   return (
-    <h1>Hello Galaxy</h1>
+    <>
+      <Hero
+        backgroundImg={'http://4.bp.blogspot.com/-R22-Cz-hZhM/UPUdbM4MlTI/AAAAAAAACbo/pfKkU98fSyE/s1600/wall-e-and-eve-best-wallpaper.jpg'}
+      >
+        <HeroContent
+          title="Etiam Dolor Ac Ligula Laoreet Porta"
+          leadText="QUISQUE SAGITTIS LOBORTIS LOREM SAGITTIS"
+          dateText="Pellentesque volutpat felis ante. Pellentesque volutpat felis ante."
+        />
+      </Hero>
+    </>
   )
 }
 
